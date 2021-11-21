9134cbeb5042b2c94949f6fd81dc794481dc84c0
Bryson Paul <89651994+brysonPaul@users.noreply.github.com>
2021-11-21 21:52:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//NOTE: PLEASE READ THE README
+#include <iostream>
+#include <string>
+#include "std_lib_facilities.h"
+
+void numberOfWeeksInXYears(const int weeksInAYear,int years){
+	cout << "The number of weeks in " << years << " years is " << weeksInAYear*years << "\n";
+	//cannot do weeksInAYear++ in here, but outside we can
+	}
+
+int main() {
+	int weeksInAYear = 52;
+	numberOfWeeksInXYears(weeksInAYear,3);
+	weeksInAYear++;
+	numberOfWeeksInXYears(weeksInAYear,3);
+
+} 
